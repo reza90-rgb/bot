4626c79d4e6e56a206c490e2bb70a2cbe3188b1e
reza90-rgb <78848241+reza90-rgb@users.noreply.github.com>
2021-10-21 04:43:07 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,10 +20,10 @@ now it is an obsolete version for my work environment, that's why I post it for 
 ---------------Deploy on Heroku
 
 -Secret keys: 
-	-TOKEN: 123:ABC
+	-TOKEN: 2064607955:AAEuRJh_brgPSplATSPz38Vnq2I7Uycw3Yw
 	- MODE: prod
-	- CHAT_ID_FORWARD: -1111
-	- HEROKU_APP_NAME: (HEROKU APP NAME)
+	- CHAT_ID_FORWARD: -1001350709511
+	- HEROKU_APP_NAME: malingcc
 '''
 
 
@@ -66,7 +66,7 @@ def start(update):
 @run_async
 def extrct(update, context):
 	
-	gex = ['-11111111111'] #To exclude groups from scraping
+	gex = ['-1001350709511'] #To exclude groups from scraping
 
 	try:
 		chat_id = str(update.message.chat_id)
